f1c7b87038da7bcf2cbfa80a98a7a06de308f3bf
quackywackyy <quackywacky14@gmail.com>
2025-10-29 22:17:29 -0500

    Minimal updates. added js code and title

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+
+
+*/
+
+
+
+
+const questionOne = document.getElementById("qu-one");
+
+function questionClicked() {
+    alert("button one pressed");
+}
+
+questionOne.addEventListener("click", questionClicked);
